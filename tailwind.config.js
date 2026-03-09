fb8a5504473c90fbce2b368f9828bf2569e3dadd
Queeneemaa <neeemmaaa@gmail.com>
2026-03-09 15:59:46 +0700

    Tailwind 1

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+  content: ['./*.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#2563EB',
+        secondary: '#16A34A',
+        background: '#F8FAFC',
+      },
+    },
+  },
+};
